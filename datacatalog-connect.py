e625b3f4714ab8d24a1ab7f7c70637ac35fa1c1f
Vivek Shinde <vivek.s.shinde@gmail.com>
2021-03-15 09:51:38 -0400

    Create datacatalog-connect.py

---
+++ datacatalog-connect.py
@@ -0,0 +1,20 @@
+from google.oauth2 import service_account
+from google.cloud import datacatalog
+
+file_name = 'path_to_your_service_account.json_file'
+
+#create datacatalog client using service account auth
+datacatalog_client = datacatalog.DataCatalogClient.from_service_account_file(file_name)
+
+scope=datacatalog.SearchCatalogRequest.Scope()
+#option to include project_ids
+scope.include_project_ids.append('dataflow1-282718')
+#option to include org_ids
+#scope.include_org_ids.append(<organization-id>)
+
+#search queries can be inserted here
+results = datacatalog_client.search_catalog(scope=scope,query='system=bigquery type=dataset')
+
+#output results
+fetched_results = [result for result in results]
+print(fetched_results)
